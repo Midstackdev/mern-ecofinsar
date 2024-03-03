3518384dd093dc9482f797c26f482faa823d5e1a
Midstackdev <midstackdev@gmail.com>
2024-03-03 20:26:53 +0000

    add client

--- server/src/app/routes/index.ts
+++ server/src/app/routes/index.ts
@@ -1,6 +1,10 @@
 import homeRoute from "./home";
 import userRoute from "./user";
 import authRoute from "./auth";
+import cleintRoute from "./cleint";
+import generalRoute from "./general";
+import managementRoute from "./management";
+import salesRoute from "./sales";
 
 import { Router } from "express";
 
@@ -9,5 +13,9 @@ const router = Router();
 router.use("/", homeRoute);
 router.use("/user", userRoute);
 router.use("/auth", authRoute);
+router.use("/client", cleintRoute);
+router.use("/general", generalRoute);
+router.use("/management", managementRoute);
+router.use("/sales", salesRoute);
 
 export const routes = router;
