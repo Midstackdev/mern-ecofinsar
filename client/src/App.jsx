2299bcc013bf83e204183013a486b6ed68552fff
Midstackdev <midstackdev@gmail.com>
2024-03-04 02:26:27 +0000

    layout

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import DashBoard from "pages/dashboard";
+import Layout from "pages/layout";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+
+const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<DashBoard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
